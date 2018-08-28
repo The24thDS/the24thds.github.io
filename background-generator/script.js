6c7cb2ecbbf8dfa41de0c5f56a15f68ea4e0345b
David Sima <me@david.mywire.org>
2018-08-28 13:05:01 +0300

    Refactoring script.js

--- background-generator/script.js
+++ background-generator/script.js
@@ -1,37 +1,40 @@
-const css = document.getElementById("js-background");
-let labels = document.querySelectorAll("[for]");
-let colors = document.querySelectorAll("[class=color]");
-const newColor = document.querySelector("[class=new-color]");
-const body = document.getElementById("gradient");
-const direction = document.getElementById("d");
-let ci = 2;
-const createColorString = () =>
-{
-	let gradientColors = "";
-	colors.forEach(function(color, i){
-		gradientColors += color.value + (i<colors.length-1?", ":"");
-		labels[i].style.background = color.value;
-	});
-	return gradientColors;
-}
+let labels = Array.prototype.slice.call(document.querySelectorAll(`[for^=color]`));
+let colors = Array.prototype.slice.call(document.querySelectorAll(`[class=color]`));
+const direction = document.getElementById(`js-direction`);
 
-const setGradient = () => {
-	colors.forEach(function(color){
-		color.addEventListener("input", setGradient);
-	});
+const createColorString = () => colors.map(color => color.value).join(`, `);
+
+const colorsInit = () =>{
+	const newColor = document.querySelector(`[class=new-color]`);
+	newColor.addEventListener(`click`, createNewColorInput);
+	colors.forEach((color, i) => {
+		color.addEventListener(`input`, setGradient);
+		labels[i].style.background = color.value;
+	})
+};
+
+const setGradient = (event) => {
+	if(event && event.target.className===`color`) event.target.parentElement.style.background = event.target.value;
+	const body = document.getElementById(`js-gradient`);
+	const css = document.getElementById(`js-background`);
 	let generatedSyntax = `linear-gradient(to ${direction.value}, ${createColorString()})`;
 	body.style.backgroundImage = generatedSyntax;
 	css.textContent = `${generatedSyntax};`;
+	return generatedSyntax;
 }
 
 const createNewColorInput = (event) => {
-	ci++;
-	event.target.insertAdjacentHTML('beforebegin', `<label for="color${ci}"><input id="color${ci}" type="color" class="color" name="color${ci}" value="#ff0034"></label>`);
-	labels = document.querySelectorAll("[for]");
-	colors = document.querySelectorAll("[class=color]");
+	let ci = colors.length + 1; //this is the number of color inputs
+	event.target.insertAdjacentHTML(`beforebegin`, `<label for="color${ci}"><input id="color${ci}" type="color" class="color" name="color${ci}" value="#ff0034"></label>`);
+	labels.push(document.querySelector(`[for=color${ci}`));
+	colors.push(document.getElementById(`color${ci}`));
+	colors[ci-1].addEventListener(`input`, setGradient);
+	labels[ci-1].style.background = colors[ci-1].value;
 	return setGradient();
 }
 
-newColor.addEventListener("click", createNewColorInput);
-direction.addEventListener("input", setGradient);
-setGradient();
+
+direction.addEventListener(`input`, setGradient);
+
+colorsInit();
+setGradient(null);
