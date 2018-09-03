20cbf9fd57dd162aef0e76ce84a5b9dfad9db02e
David Sima <me@david.mywire.org>
2018-09-03 11:03:06 +0300

    Adding deleteColor function

--- background-generator/script.js
+++ background-generator/script.js
@@ -11,6 +11,48 @@ const randomHex = () => {
 	return color;
 }
 
+const setGradient = (event) => {
+	if(event && event.target.className===`color`) event.target.parentElement.style.background = event.target.value;
+	const body = document.getElementById(`js-gradient`);
+	const css = document.getElementById(`js-background`);
+	let generatedSyntax = `linear-gradient(to ${direction.value}, ${createColorString()})${(direction.value==='top'||direction.value==='bottom')? ' no-repeat fixed' : ''}`;
+	body.style.background = generatedSyntax;
+	css.textContent = `background: ${generatedSyntax};`;
+	return generatedSyntax;
+}
+
+
+const createNewColorInput = (event) => {
+	let ci = colors.length + 1; //this is the number of color inputs
+	event.target.insertAdjacentHTML(`beforebegin`, `<label for="color${ci}"><input id="color${ci}" type="color" class="color" name="color${ci}" value="#ff0034"></label>`);
+	labels.push(document.querySelector(`[for=color${ci}`));
+	colors.push(document.getElementById(`color${ci}`));
+	colors[ci-1].addEventListener(`input`, setGradient);
+	colors[ci-1].addEventListener(`contextmenu`, deleteColor);
+	labels[ci-1].style.background = colors[ci-1].value = randomHex();
+	return setGradient();
+}
+
+const deleteColor = (event) => {
+	event.preventDefault();
+	let colorIndex = colors.findIndex((el)=>el===event.target);
+	colors.splice(colorIndex, 1);
+	labels.splice(colorIndex, 1);
+	event.target.parentElement.remove();
+	colors.forEach((color, i)=>{
+		color.setAttribute(`id`, `color${i+1}`);
+		color.setAttribute(`name`, `color${i+1}`);
+		labels[i].setAttribute(`for`, `color${i+1}`);
+	});
+	setGradient(null);
+	return false;
+}
+
+const randomGradient = () => {
+	colors.forEach((color, i)=>{labels[i].style.background = color.value = randomHex();});
+	setGradient(null);
+}
+
 const gradientInit = () =>{
 	const randomButton = document.getElementById(`js-random-button`);
 	const newColor = document.querySelector(`[class=new-color]`);
@@ -25,34 +67,10 @@ const gradientInit = () =>{
 	direction.addEventListener(`input`, setGradient);
 	colors.forEach((color, i) => {
 		color.addEventListener(`input`, setGradient);
+		color.addEventListener('contextmenu', deleteColor);
 		labels[i].style.background = color.value = randomHex();
 	});
 	setGradient(null);
 };
-
-const setGradient = (event) => {
-	if(event && event.target.className===`color`) event.target.parentElement.style.background = event.target.value;
-	const body = document.getElementById(`js-gradient`);
-	const css = document.getElementById(`js-background`);
-	let generatedSyntax = `linear-gradient(to ${direction.value}, ${createColorString()})${(direction.value==='top'||direction.value==='bottom')? ' no-repeat fixed' : ''}`;
-	body.style.background = generatedSyntax;
-	css.textContent = `background: ${generatedSyntax};`;
-	return generatedSyntax;
-}
-
-const createNewColorInput = (event) => {
-	let ci = colors.length + 1; //this is the number of color inputs
-	event.target.insertAdjacentHTML(`beforebegin`, `<label for="color${ci}"><input id="color${ci}" type="color" class="color" name="color${ci}" value="#ff0034"></label>`);
-	labels.push(document.querySelector(`[for=color${ci}`));
-	colors.push(document.getElementById(`color${ci}`));
-	colors[ci-1].addEventListener(`input`, setGradient);
-	labels[ci-1].style.background = colors[ci-1].value = randomHex();
-	return setGradient();
-}
-
-const randomGradient = () => {
-	colors.forEach((color, i)=>{labels[i].style.background = color.value = randomHex();});
-	setGradient(null);
-}
 
 gradientInit();
